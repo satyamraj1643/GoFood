0d4e61e028684849a348a53d0411a7e710488bfe
satyamraj1643 <satyamraj1643@gmail.com>
2024-01-07 18:43:33 +0530

    Updating

--- backend/routes/fetchItems.js
+++ backend/routes/fetchItems.js
@@ -1,29 +1,18 @@
 const express = require('express');
-const {mongoose} = require('mongoose')
+const mongoose = require('mongoose'); // Fix the import statement
 const router = express.Router();
 
+router.get('/', async (req, res) => {
+  try {
+    const collection = mongoose.connection.collection('FoodItems');
+    const cursor = collection.find({});
+    const data = await cursor.toArray();
 
-router.get('/' , async(req,res)=>{
-    var fetched_data;
-
-      try{
-
-         fetched_data  = await mongoose.connection.collection('FoodItems');
-          
-
-      }
-      catch{
-           console.log('An error ocurred while fetching data.');
-           res.json({status:'failure'})
-      }
-
-
-
-      const data = await fetched_data.find({}).toArray();
-
-      res.json({status:'success', FoodItems: data});
-        
-})
-
+    res.json({ status: 'success', FoodItems: data });
+  } catch (error) {
+    console.log('An error occurred while fetching data:', error);
+    res.json({ status: 'failure' });
+  }
+});
 
 module.exports = router;
