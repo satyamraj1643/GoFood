b4da8ef4c142e3cd4b3587203115a1aab6ef5a79
satyamraj1643 <satyamraj1643@gmail.com>
2024-01-07 18:41:31 +0530

    Updating

--- backend/routes/fetchCategory.js
+++ backend/routes/fetchCategory.js
@@ -1,29 +1,17 @@
 const express = require('express');
-const {mongoose} = require('mongoose')
+const { mongoose } = require('mongoose')
 const router = express.Router();
 
+router.get('/', async (req, res) => {
+  try {
+    const collection = mongoose.connection.collection('FoodCategory');
+    const data = await collection.find({}).toArray();
 
-router.get('/' , async(req,res)=>{
-    var fetched_data;
-
-      try{
-
-         fetched_data  = await mongoose.connection.collection('FoodCategory');
-          
-
-      }
-      catch{
-           console.log('An error ocurred while fetching data.');
-           res.json({status:'failure'})
-      }
-
-
-
-      const data = await fetched_data.find({}).toArray();
-
-      res.json({status:'success', FoodCategory: data});
-        
-})
-
+    res.json({ status: 'success', FoodCategory: data });
+  } catch (error) {
+    console.log('An error occurred while fetching data:', error);
+    res.json({ status: 'failure' });
+  }
+});
 
 module.exports = router;
