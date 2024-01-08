6a76dca5dd4bf614ae59a81c110a4939b5b76c05
satyamraj1643 <satyamraj1643@gmail.com>
2024-01-08 21:50:58 +0530

    Avoiding vim

--- backend/app.js
+++ backend/app.js
@@ -1,10 +1,11 @@
+require("dotenv").config();
 const express = require('express');
 const {dbconnect} = require('./database/db.js')
 const app = express();
 const cors = require('cors');
 
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 dbconnect();
 
 
@@ -27,7 +28,7 @@ app.use((req,res,next)=>{
 //     credentials: true
     
 // }));
- app.use(cors());
+ //app.use(cors());
 
 
 app.get('/', (req,res)=>{
--- backend/database/dburl.js
+++ backend/database/dburl.js
@@ -1,4 +1,6 @@
-const url = 'mongodb+srv://gofood:satyam%40123A@gofood.ihfz3mr.mongodb.net/GoFood'
+ //const url = 'mongodb+srv://gofood:satyam%40123A@gofood.ihfz3mr.mongodb.net/GoFood'
+
+const url = process.env.MONGO_URI ||  'mongodb+srv://gofood:satyam%40123A@gofood.ihfz3mr.mongodb.net/GoFood'
 module.exports = {
     url
 }
