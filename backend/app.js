bd69d6aab239890d7d5858d3a170cd808f92ed0a
satyamraj1643 <satyamraj1643@gmail.com>
2024-01-08 23:22:40 +0530

    Finally backend deployed

--- backend/app.js
+++ backend/app.js
@@ -32,7 +32,7 @@ app.use((req,res,next)=>{
 
 
 app.get('/', (req,res)=>{
-    res.send("Hello React! Deployed from vercel.");
+    res.send("Hello React! Deployed from AWS.");
 })
 
  app.use(express.json());
