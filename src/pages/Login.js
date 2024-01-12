e1262c78f11cd68be104c4d3346c34c486a2174a
Satyam Raj <satyamraj1643@gmail.com>
2024-01-12 20:36:17 +0530

    Update Login.js

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -38,7 +38,7 @@ function Login() {
     let response = null;
     try {
 
-      response = await fetch('http://13.232.155.252:5000/api/loginuser', {
+      response = await fetch('http://3.111.29.8:5000/api/loginuser', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
