1c15cb4b7c776ba93ecfea102a5e47cee3157b32
Satyam Raj <satyamraj1643@gmail.com>
2024-01-18 18:11:42 +0530

    hosting

--- backend/app.js
+++ backend/app.js
@@ -1,70 +1,36 @@
 require("dotenv").config();
 const express = require('express');
-const {dbconnect} = require('./database/db.js')
+const { dbconnect } = require('./database/db.js');
 const app = express();
 const cors = require('cors');
 
-
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT;
 dbconnect();
 
-
-
-// app.use((req,res,next)=>{
-//     res.setHeader("Acess-Control-Allow-Origin", "http://localhost:5000");
-//     res.header(
-//         "Access-Control-Allow-Headers",
-//         "Origin, X-Requested-With, Content-Type, Accept"
-//     );
-
-//     next();
-// })
-
-
-// app.use(cors({
-
-//     origin: ["https://gofood-1whq.vercel.app"], 
-//     methods: ["POST", "GET"],
-//     credentials: true
-    
-// }));
 const allowedOrigins = ['https://gofood-wq3s.onrender.com'];
 
 app.use(cors({
   origin: function (origin, callback) {
-    if (!origin || allowedOrigins.includes(origin)!== -1) {
+    if (!origin || allowedOrigins.includes(origin)) {
       callback(null, true);
     } else {
       callback(new Error('Not allowed by CORS'));
     }
   },
+  credentials: true, // if your API uses credentials
+  exposedHeaders: ['custom-header1', 'custom-header2'], // specify your custom headers
 }));
 
-app.use(cors());
+app.get('/', (req, res) => {
+  res.send("Hello peeps");
+});
 
-app.get('/', (req,res)=>{
-    res.send("Hello peeps");
-})
+app.use(express.json());
+app.use('/api/createuser', require('./routes/createUser.js'));
+app.use('/api/loginuser', require('./routes/logInUser.js'));
+app.use('/api/fetchitems', require('./routes/fetchItems.js'));
+app.use('/api/fetchcategory', require('./routes/fetchCategory.js'));
 
- app.use(express.json());
- app.use('/api/createuser', require('./routes/createUser.js'));
- app.use('/api/loginuser', require('./routes/logInUser.js') );
- app.use('/api/fetchitems', require('./routes/fetchItems.js'));
- app.use('/api/fetchcategory', require('./routes/fetchCategory.js'));
-
-
-
-
-
-app.listen(PORT, ()=>{
-    console.log("App started on Port: ", PORT );
-})
-
-
-
-
-
-
-
-
-
+app.listen(PORT, () => {
+  console.log("App started on Port: ", PORT);
+});
